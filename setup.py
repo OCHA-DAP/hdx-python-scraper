8996ef1956210ddb05c63a7ae4a31918e2f2268e
Mike <rans@un.org>
2021-03-30 05:49:29 +1300

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from hdx.utilities import CleanCommand, PackageCommand, PublishCommand
 from hdx.utilities.loader import load_file_to_str
 from setuptools import setup, find_packages
 
-requirements = ['hdx-python-api>=4.9.5',
+requirements = ['hdx-python-api>=5.0.0',
                 'olefile',
                 'pygsheets',
                 'regex']
