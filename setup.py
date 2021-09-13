02ec88b62eace2e4c9b8da787703883c4668f07e
Mike <rans@un.org>
2021-09-14 11:52:01 +1200

    Move most of setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -2,50 +2,17 @@ from os.path import join
 
 from hdx.utilities import CleanCommand, PackageCommand, PublishCommand
 from hdx.utilities.loader import load_file_to_str
-from setuptools import find_namespace_packages, setup
-
-requirements = ["hdx-python-api>=5.3.1", "gspread", "olefile", "regex"]
-
-extras_requirements = {"pandas": ["pandas>=1.2.4"]}
-
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "Natural Language :: English",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-]
+from setuptools import setup
 
 PublishCommand.version = load_file_to_str(
     join("src", "hdx", "scraper", "version.txt"), strip=True
 )
 
 setup(
-    name="hdx-python-scraper",
-    description="HDX Python Scraper Library",
-    license="MIT",
-    url="https://github.com/OCHA-DAP/hdx-python-scraper",
     version=PublishCommand.version,
-    author="Michael Rans",
-    author_email="rans@email.com",
-    keywords=["HDX", "API", "library"],
-    long_description=load_file_to_str("README.md"),
-    long_description_content_type="text/markdown",
-    packages=find_namespace_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    zip_safe=True,
-    classifiers=classifiers,
-    install_requires=requirements,
-    extras_require=extras_requirements,
     cmdclass={
         "clean": CleanCommand,
         "package": PackageCommand,
-        "publish": PublishCommand,
-    },
+        "publish": PublishCommand
+    }
 )
