4b23e23707660a68dd1e104ab81e11e0f4a047bb
Mike <rans@un.org>
2022-03-21 14:10:09 +1300

    Aggregator now supports HXL hash tags

--- src/hdx/scraper/configurable/aggregator.py
+++ src/hdx/scraper/configurable/aggregator.py
@@ -25,45 +25,71 @@ class Aggregator(BaseScraper):
         adm_aggregation: Union[Dict, List],
         input_values: Dict,
         aggregation_scrapers: [List[BaseScraper]],
+        use_hxl: bool,
     ):
         super().__init__(name, datasetinfo, headers)
         self.adm_aggregation = adm_aggregation
         self.input_values = input_values
         self.aggregation_scrapers = aggregation_scrapers
+        self.use_hxl = use_hxl
 
     @classmethod
     def get_scrapers(
-        cls, configuration, input_level, output_level, adm_aggregation, runner
+        cls,
+        configuration,
+        input_level,
+        output_level,
+        adm_aggregation,
+        runner,
+        use_hxl=True,
     ):
         input_results = runner.get_results(levels=input_level, has_run=False)
         aggregation_scrapers = list()
         if not input_results:
             return aggregation_scrapers
+        if use_hxl:
+            main_index = 1
+            header_or_hxltag_str = "hxltag"
+            lookup = "hxltags"
+        else:
+            main_index = 0
+            header_or_hxltag_str = "header"
+            lookup = "headers"
         input_results = input_results[input_level]
         input_headers = input_results["headers"]
         input_vals = input_results["values"]
         input_values = dict()
-        for index, input_header in enumerate(input_headers[0]):
-            input_values[input_header] = input_vals[index]
+        for index, input_header_or_hxltag in enumerate(
+            input_headers[main_index]
+        ):
+            input_values[input_header_or_hxltag] = input_vals[index]
         if not isinstance(adm_aggregation, dict):
             adm_aggregation = {x: ("value",) for x in adm_aggregation}
-        for header, process_info in configuration.items():
-            name = f"{slugify(header.lower(), separator='_')}_{output_level}"
-            config_headers = process_info.get("headers")
-            if config_headers:
+        for header_or_hxltag, process_info in configuration.items():
+            process_info = copy.deepcopy(process_info)
+            name = f"{slugify(header_or_hxltag.lower(), separator='_')}_{output_level}"
+            config_headers_or_hxltags = process_info.get(lookup)
+            if config_headers_or_hxltags:
                 exists = True
-                for i, config_header in enumerate(config_headers):
+                for i, config_header_or_hxltag in enumerate(
+                    config_headers_or_hxltags
+                ):
                     try:
-                        input_headers[0].index(config_header)
+                        input_headers[main_index].index(
+                            config_header_or_hxltag
+                        )
                     except ValueError:
                         logger.error(
-                            f"{output_level} header {header} not found in {input_level} headers!"
+                            f"{output_level} {header_or_hxltag_str} {header_or_hxltag} not found in {input_level} {lookup}!"
                         )
                         exists = False
                         break
                 if not exists:
                     continue
-                headers = ((header,), (process_info["hxltag"],))
+                if use_hxl:
+                    headers = ((process_info["header"],), (header_or_hxltag,))
+                else:
+                    headers = ((header_or_hxltag,), (process_info["hxltag"],))
                 scraper = cls(
                     name,
                     process_info,
@@ -71,15 +97,26 @@ class Aggregator(BaseScraper):
                     adm_aggregation,
                     input_values,
                     aggregation_scrapers,
+                    use_hxl,
                 )
                 aggregation_scrapers.append(scraper)
             else:
                 try:
-                    index = input_headers[0].index(header)
-                    process_info = copy.deepcopy(process_info)
-                    process_info["headers"] = (header,)
-                    header = process_info.get("rename", header)
-                    headers = ((header,), (input_headers[1][index],))
+                    index = input_headers[main_index].index(header_or_hxltag)
+                    process_info[lookup] = (header_or_hxltag,)
+                    header_or_hxltag = process_info.get(
+                        "rename", header_or_hxltag
+                    )
+                    if use_hxl:
+                        headers = (
+                            (input_headers[0][index],),
+                            (header_or_hxltag,),
+                        )
+                    else:
+                        headers = (
+                            (header_or_hxltag,),
+                            (input_headers[1][index],),
+                        )
                     scraper = cls(
                         name,
                         process_info,
@@ -87,11 +124,12 @@ class Aggregator(BaseScraper):
                         adm_aggregation,
                         input_values,
                         aggregation_scrapers,
+                        use_hxl,
                     )
                     aggregation_scrapers.append(scraper)
                 except ValueError:
                     logger.error(
-                        f"{output_level} header {header} not found in {input_level} headers!"
+                        f"{output_level} {header_or_hxltag_str} {header_or_hxltag} not found in {input_level} {lookup}!"
                     )
         return aggregation_scrapers
 
@@ -192,15 +230,19 @@ class Aggregator(BaseScraper):
             else:
                 population_str = "self.population_lookup[population_key]"
             arbitrary_string = "#pzbgvjh"
-            headers = list()
+            headers_or_hxltags = list()
+            if self.use_hxl:
+                index = 1
+            else:
+                index = 0
             for aggregation_scraper in self.aggregation_scrapers:
-                headers.append(
-                    aggregation_scraper.get_headers(output_level)[0][0]
+                headers_or_hxltags.append(
+                    aggregation_scraper.get_headers(output_level)[index][0]
                 )
             # Indices of list sorted by length
             sorted_len_indices = sorted(
-                range(len(headers)),
-                key=lambda k: len(headers[k]),
+                range(len(headers_or_hxltags)),
+                key=lambda k: len(headers_or_hxltags[k]),
                 reverse=True,
             )
             for output_adm, valuelist in output_values.items():
@@ -209,7 +251,7 @@ class Aggregator(BaseScraper):
                     aggregation_scraper = self.aggregation_scrapers[i]
                     values = aggregation_scraper.get_values(output_level)[0]
                     value = values.get(output_adm, "")
-                    toeval = toeval.replace(headers[i], str(value))
+                    toeval = toeval.replace(headers_or_hxltags[i], str(value))
                 toeval = toeval.replace(arbitrary_string, population_str)
                 total = eval(toeval)
                 output_values[output_adm] = total
@@ -221,9 +263,12 @@ class Aggregator(BaseScraper):
         output_valdicts = self.get_values(output_level)
         output_values = output_valdicts[0]
         input_valdicts = list()
-        input_headers = self.datasetinfo["headers"]
-        for input_header in input_headers:
-            input_valdicts.append(self.input_values[input_header])
+        if self.use_hxl:
+            input_headers_or_hxltags = self.datasetinfo["hxltags"]
+        else:
+            input_headers_or_hxltags = self.datasetinfo["headers"]
+        for input_header_or_hxltag in input_headers_or_hxltags:
+            input_valdicts.append(self.input_values[input_header_or_hxltag])
         found_adms = set()
         for input_values in input_valdicts:
             for input_adm in input_values:
--- tests/hdx/scraper/test_scrapers_aggregation.py
+++ tests/hdx/scraper/test_scrapers_aggregation.py
@@ -10,12 +10,13 @@ from .conftest import check_scrapers, run_check_scraper, run_check_scrapers
 
 
 class TestScrapersAggregation:
-    def test_get_aggregation(self, configuration, fallbacks):
+    def test_get_aggregation_hxl(self, configuration, fallbacks):
         BaseScraper.population_lookup = dict()
         with Download(user_agent="test") as downloader:
             today = parse_date("2020-10-01")
             adminone = AdminOne(configuration)
             level = "national"
+            configuration_hxl = configuration["aggregation_hxl"]
             scraper_configuration = configuration[f"scraper_{level}"]
             iso3s = ("AFG", "MMR")
             runner = Runner(iso3s, adminone, downloader, dict(), today)
@@ -48,7 +49,7 @@ class TestScrapersAggregation:
             adm_aggregation = {"AFG": ("ROAP",), "MMR": ("ROAP",)}
 
             level = "regional"
-            aggregator_configuration = configuration["aggregation_sum"]
+            aggregator_configuration = configuration_hxl["aggregation_sum"]
             scrapers = Aggregator.get_scrapers(
                 aggregator_configuration,
                 "national",
@@ -72,7 +73,7 @@ class TestScrapersAggregation:
                 source_urls=list(),
             )
 
-            aggregator_configuration = configuration["aggregation_mean"]
+            aggregator_configuration = configuration_hxl["aggregation_mean"]
             scrapers = Aggregator.get_scrapers(
                 aggregator_configuration,
                 "national",
@@ -95,7 +96,7 @@ class TestScrapersAggregation:
                 source_urls=list(),
             )
 
-            aggregator_configuration = configuration["aggregation_range"]
+            aggregator_configuration = configuration_hxl["aggregation_range"]
             scrapers = Aggregator.get_scrapers(
                 aggregator_configuration,
                 "national",
@@ -177,7 +178,7 @@ class TestScrapersAggregation:
             )
 
             level = "regional"
-            aggregator_configuration = configuration["aggregation_global"]
+            aggregator_configuration = configuration_hxl["aggregation_global"]
             adm_aggregation = ("AFG", "PSE")
             scrapers = Aggregator.get_scrapers(
                 aggregator_configuration,
@@ -185,6 +186,216 @@ class TestScrapersAggregation:
                 level,
                 adm_aggregation,
                 runner,
+            )
+            names = runner.add_customs(scrapers, add_to_run=True)
+            assert names == [
+                "population_regional",
+                "affected_infected_per100000_regional",
+                "affected_infected_perpop_regional",
+            ]
+            pop = 42727060
+            headers = (
+                ["Population", "CasesPer100000", "CasesPerPopulation"],
+                [
+                    "#population",
+                    "#affected+infected+per100000",
+                    "#affected+infected+perpop",
+                ],
+            )
+            values = [{"value": pop}, {"value": "459.42"}, {"value": "1.0752"}]
+            run_check_scrapers(
+                names,
+                runner,
+                level,
+                headers,
+                values,
+                list(),
+                population_lookup=national_values[0] | {"global": pop},
+                source_urls=source_urls,
+            )
+
+    def test_get_aggregation_nohxl(self, configuration, fallbacks):
+        BaseScraper.population_lookup = dict()
+        with Download(user_agent="test") as downloader:
+            today = parse_date("2020-10-01")
+            adminone = AdminOne(configuration)
+            level = "national"
+            configuration_nohxl = configuration["aggregation_nohxl"]
+            scraper_configuration = configuration[f"scraper_{level}"]
+            iso3s = ("AFG", "MMR")
+            runner = Runner(iso3s, adminone, downloader, dict(), today)
+            runner.add_configurables(scraper_configuration, level)
+
+            name = "population"
+            headers = (["Population"], ["#population"])
+            national_values = [{"AFG": 38041754, "MMR": 54045420}]
+            sources = [
+                (
+                    "#population",
+                    "2020-10-01",
+                    "World Bank",
+                    "https://data.humdata.org/organization/world-bank-group",
+                )
+            ]
+            run_check_scraper(
+                name,
+                runner,
+                level,
+                headers,
+                national_values,
+                sources,
+                population_lookup=national_values[0],
+                source_urls=[
+                    "https://data.humdata.org/organization/world-bank-group"
+                ],
+            )
+
+            adm_aggregation = {"AFG": ("ROAP",), "MMR": ("ROAP",)}
+
+            level = "regional"
+            aggregator_configuration = configuration_nohxl["aggregation_sum"]
+            scrapers = Aggregator.get_scrapers(
+                aggregator_configuration,
+                "national",
+                level,
+                adm_aggregation,
+                runner,
+                use_hxl=False,
+            )
+            scraper_names = runner.add_customs(scrapers, add_to_run=True)
+            name = "population_regional"
+            assert scraper_names == [name]
+
+            values = [{"ROAP": 92087174}]
+            run_check_scraper(
+                name,
+                runner,
+                level,
+                headers,
+                values,
+                list(),
+                population_lookup=national_values[0] | values[0],
+                source_urls=list(),
+            )
+
+            aggregator_configuration = configuration_nohxl["aggregation_mean"]
+            scrapers = Aggregator.get_scrapers(
+                aggregator_configuration,
+                "national",
+                level,
+                adm_aggregation,
+                runner,
+                use_hxl=False,
+            )
+            runner.add_customs(scrapers, add_to_run=True)
+
+            level = "regional"
+            values = [{"ROAP": 46043587}]
+            run_check_scraper(
+                name,
+                runner,
+                level,
+                headers,
+                values,
+                list(),
+                population_lookup=national_values[0] | values[0],
+                source_urls=list(),
+            )
+
+            aggregator_configuration = configuration_nohxl["aggregation_range"]
+            scrapers = Aggregator.get_scrapers(
+                aggregator_configuration,
+                "national",
+                level,
+                adm_aggregation,
+                runner,
+                use_hxl=False,
+            )
+            runner.add_customs(scrapers, add_to_run=True)
+
+            level = "regional"
+            values = [{"ROAP": "38041754-54045420"}]
+            run_check_scraper(
+                name,
+                runner,
+                level,
+                headers,
+                values,
+                list(),
+                source_urls=list(),
+            )
+
+            level = "national"
+            BaseScraper.population_lookup = dict()
+            iso3s = ("AFG", "PSE")
+            runner = Runner(iso3s, adminone, downloader, dict(), today)
+            runner.add_configurables(scraper_configuration, level)
+
+            names = ("population", "who_national")
+            headers = (
+                [
+                    "Population",
+                    "CasesPer100000",
+                    "DeathsPer100000",
+                ],
+                [
+                    "#population",
+                    "#affected+infected+per100000",
+                    "#affected+killed+per100000",
+                ],
+            )
+            national_values = [
+                {"AFG": 38041754, "PSE": 4685306},
+                {"AFG": "96.99", "PSE": "362.43"},
+                {"AFG": "3.41", "PSE": "1.98"},
+            ]
+            sources = [
+                (
+                    "#population",
+                    "2020-10-01",
+                    "World Bank",
+                    "https://data.humdata.org/organization/world-bank-group",
+                ),
+                (
+                    "#affected+infected+per100000",
+                    "2020-08-06",
+                    "WHO",
+                    "tests/fixtures/WHO-COVID-19-global-data.csv",
+                ),
+                (
+                    "#affected+killed+per100000",
+                    "2020-08-06",
+                    "WHO",
+                    "tests/fixtures/WHO-COVID-19-global-data.csv",
+                ),
+            ]
+            source_urls = [
+                "https://data.humdata.org/organization/world-bank-group",
+                "tests/fixtures/WHO-COVID-19-global-data.csv",
+            ]
+            runner.run(names)
+            check_scrapers(
+                names,
+                runner,
+                level,
+                headers,
+                national_values,
+                sources,
+                source_urls=source_urls,
+            )
+
+            level = "regional"
+            aggregator_configuration = configuration_nohxl[
+                "aggregation_global"
+            ]
+            adm_aggregation = ("AFG", "PSE")
+            scrapers = Aggregator.get_scrapers(
+                aggregator_configuration,
+                "national",
+                level,
+                adm_aggregation,
+                runner,
+                use_hxl=False,
             )
             names = runner.add_customs(scrapers, add_to_run=True)
             assert names == [
