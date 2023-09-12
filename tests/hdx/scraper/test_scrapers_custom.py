3191fadbd05f3a09edff5ccf276871fc32969645
Mike <rans@un.org>
2023-09-13 10:29:39 +1200

    Fix test

--- tests/hdx/scraper/test_scrapers_custom.py
+++ tests/hdx/scraper/test_scrapers_custom.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from datetime import datetime, timezone
 
 from .affected_targeted_reached import AffectedTargetedReached
 from .conftest import check_scraper, check_scrapers
@@ -42,16 +43,32 @@ class TestScrapersCustom:
         ]
         check_scraper(name, runner, "national", headers, values, sources)
         hapi_metadata = runner.get_hapi_metadata()[0]
-        assert hapi_metadata == [
-            {
-                "code": "6a41be98-75b9-4365-9ea3-e33d0dd2668b",
-                "title": "Global School Closures COVID-19",
-                "provider_code": "18f2d467-dcf8-4b7e-bffa-b3c338ba3a7c",
-                "provider_name": "unesco",
-                "hdx_link": "https://data.humdata.org/dataset/global-school-closures-covid19",
-                "api_link": "https://data.humdata.org/api/3/action/package_show?id=global-school-closures-covid19",
-            }
-        ]
+        assert hapi_metadata == {
+            "hdx_id": "6a41be98-75b9-4365-9ea3-e33d0dd2668b",
+            "hdx_stub": "global-school-closures-covid19",
+            "provider_code": "18f2d467-dcf8-4b7e-bffa-b3c338ba3a7c",
+            "provider_name": "unesco",
+            "reference_period": {
+                "enddate": datetime(
+                    2022, 4, 30, 23, 59, 59, tzinfo=timezone.utc
+                ),
+                "enddate_str": "2022-04-30T23:59:59+00:00",
+                "ongoing": False,
+                "startdate": datetime(2020, 2, 16, 0, 0, tzinfo=timezone.utc),
+                "startdate_str": "2020-02-16T00:00:00+00:00",
+            },
+            "resource": {
+                "download_url": "https://data.humdata.org/dataset/6a41be98-75b9-4365-9ea3-e33d0dd2668b/resource/3b5baa74-c928-4cbc-adba-bf543c5d3050/download/covid_impact_education.csv",
+                "filename": "School Closures",
+                "format": "csv",
+                "hdx_id": "3b5baa74-c928-4cbc-adba-bf543c5d3050",
+                "update_date": datetime(
+                    2022, 4, 4, 9, 56, 5, tzinfo=timezone.utc
+                ),
+            },
+            "title": "Global School Closures COVID-19",
+        }
+
         headers = (["No. closed countries"], ["#status+country+closed"])
         values = [{"ROAP": 1}]
         sources = [
