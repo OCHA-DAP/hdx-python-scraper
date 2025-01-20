c2bc4dffcc73a81224e59abce130fbc370caa98f
mcarans <rans@email.com>
2025-01-20 15:46:18 +1300

    Refactor sector and add org_type

---
+++ tests/hdx/scraper/framework/utilities/test_lookup.py
@@ -0,0 +1,16 @@
+from hdx.scraper.framework.utilities.org_type import OrgType
+from hdx.scraper.framework.utilities.sector import Sector
+
+
+class TestLookup:
+    def test_sector(self, configuration):
+        sector = Sector()
+        assert sector.get_code("child protection") == "PRO-CPN"
+        assert (
+            sector.get_code("gestion des sites daccueil temporaires") == "SHL"
+        )
+
+    def test_org_type(self, configuration):
+        org_type = OrgType()
+        assert org_type.get_code("embassy") == "434"
+        assert org_type.get_code("ong int") == "437"
